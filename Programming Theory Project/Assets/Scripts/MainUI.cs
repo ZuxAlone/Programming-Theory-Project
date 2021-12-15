e95d9db94f9d59376c26d1a4a8f8c81d7b758381
ZuxAlone <rmagdev20@gmail.com>
2021-12-14 21:31:45 -0500

    Back to menu button
    - Added a back to menu button on the main scene

--- Programming Theory Project/Assets/Scripts/MainUI.cs
+++ Programming Theory Project/Assets/Scripts/MainUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class MainUI : MonoBehaviour
@@ -12,5 +13,10 @@ public class MainUI : MonoBehaviour
     public void UpdatePoints()
     {
         pointsText.text = "Points: " + player.Points;
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
